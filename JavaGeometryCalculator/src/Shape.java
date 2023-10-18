ce4da0a4bf3057c5c0d5e9cb575337fef04d9388
The-wise-ant <113945824+The-wise-ant@users.noreply.github.com>
2023-10-19 00:44:11 +0200

    "added a Shape abstract class"

---
+++ JavaGeometryCalculator/src/Shape.java
@@ -0,0 +1,17 @@
+
+public abstract class Shape {
+
+	protected double area;
+	 protected double perimeter;
+
+	public abstract double getPerimeter();
+
+	public abstract double getArea();
+
+	public abstract void setPerimeter(double newPerimeter);
+
+	public abstract void setArea(double newArea);
+
+
+
+}
