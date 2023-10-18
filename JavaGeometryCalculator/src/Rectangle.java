78eb66d5e95b3eaf835678b92315cd54f9e18397
The-wise-ant <113945824+The-wise-ant@users.noreply.github.com>
2023-10-18 21:11:23 +0200

    "1.added enum for geometry constants;  2.added abstract class Shape;  3. improved the way internal state changes are handled  4. added circle class"

--- JavaGeometryCalculator/src/Rectangle.java
+++ JavaGeometryCalculator/src/Rectangle.java
@@ -1,28 +1,75 @@
 
-public class Rectangle {
+public class Rectangle extends Shape {
 
 	private double width;
 	private double length;
+
+	private double diagonal;
 
 	Rectangle(double width, double length) {
 
 		this.width = width;
 		this.length = length;
 
+		this.area = width * length;
+		this.perimeter = 2 * (width * length);
+		this.diagonal = Math.sqrt((Math.pow(length, 2) + Math.pow(width, 2)));
+
 	}
 
+	@Override
 	public double getArea() {
-		return width * length;
+		return this.area;
 	}
 
+	@Override
 	public double getPerimeter() {
 
-		return 2 * (width * length);
+		return this.perimeter;
 	}
 
 	public double getDiagonal() {
 
-		return Math.sqrt((Math.pow(length, 2) + Math.pow(width, 2)));
+		return this.diagonal;
 	}
+	
+
+	@Override
+	public void setPerimeter(double newPerimeter) {
+	    // Calculate the new width and length based on the given perimeter
+	    this.width = newPerimeter / (2 + this.length);
+	    this.length = newPerimeter / (2 + this.width);
+
+	    // Update the perimeter and area accordingly
+	    this.perimeter = newPerimeter;
+	    this.area = this.width * this.length;
+	    
+	    // Calculate and set the new diagonal
+	    this.diagonal = Math.sqrt(Math.pow(this.length, 2) + Math.pow(this.width, 2));
+	}
+
+	
+	
+
+	@Override
+	public void setArea(double newArea) {
+	    // Calculate the new width or length based on the given area
+	    if (newArea > 0) {
+	        double newWidth = Math.sqrt(newArea * width / length);
+	        double newLength = newArea / newWidth;
+	        
+	        // Update the width and length
+	        this.width = newWidth;
+	        this.length = newLength;
+
+	        // Recalculate the perimeter and diagonal
+	        this.perimeter = 2 * (this.width + this.length);
+	        this.diagonal = Math.sqrt(Math.pow(this.length, 2) + Math.pow(this.width, 2));
+
+	        // Set the new area
+	        this.area = newArea;
+	    }
+	}
+
 
 }
--- JavaGeometryCalculator/src/Square.java
+++ JavaGeometryCalculator/src/Square.java
@@ -1,40 +1,59 @@
 
-public class Square {
+public class Square extends Shape {
 	private double side;
+	
+	////CONSTRUCTORS//////
 
 	public Square(double side) {
 		this.side = side;
+		this.perimeter = side * 4;
+		this.area = side * side;
 	}
 
 	public Square(double value, String parameterType) {
 		if (parameterType.equalsIgnoreCase("area")) {
-			this.side = Math.sqrt(value);
+			setArea(value);
+
 		} else if (parameterType.equalsIgnoreCase("perimeter")) {
-			this.side = value / 4;
+			setPerimeter(value);
 		}
 	}
 
-	public double getArea() {
-		return side * side;
-	}
-
-	public double getPerimeter() {
-		return 4 * side;
-	}
+	//// GETTERS///////
 
 	public double getSide() {
-		return side;
+		return this.side;
 	}
+
+	@Override
+	public double getArea() {
+		return this.area;
+	}
+
+	@Override
+	public double getPerimeter() {
+		return this.perimeter;
+	}
+
+	///// SETTERS///////
 
 	public void setSide(double newSide) {
-		side = newSide;
+		this.side = newSide;
+		this.perimeter = newSide * 4;
+		this.area = newSide * newSide;
 	}
 
+	@Override
 	public void setPerimeter(double newPerimeter) {
-		side = newPerimeter / 4;
+		this.perimeter = newPerimeter;
+		this.side = newPerimeter / 4;
+		this.area = (newPerimeter / 4) * (newPerimeter / 4);
 	}
 
+	@Override
 	public void setArea(double newArea) {
-		side = Math.sqrt(newArea);
+		this.side = Math.sqrt(newArea);
+		this.perimeter = Math.sqrt(newArea) * 4;
 	}
+
 }
