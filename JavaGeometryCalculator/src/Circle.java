e636b4c07dd4b8503705a4268b9e957ab9ffb37b
The-wise-ant <113945824+The-wise-ant@users.noreply.github.com>
2023-10-20 05:21:50 +0200

    "added Circle class"

---
+++ JavaGeometryCalculator/src/Circle.java
@@ -0,0 +1,63 @@
+
+public class Circle extends Shape {
+
+	double piValue = GeometryConstants.PI.getValue();
+	private double radius;
+	private double diameter;
+
+	public Circle(double radius) {
+
+		this.radius = radius;
+		this.diameter = 2 * radius;
+		this.perimeter = 2 * (piValue * radius);
+		this.area = piValue * (radius * radius);
+	}
+
+	//////// GETTERS//////
+	public double getArea() {
+		return this.area;
+	}
+
+	public double getCircumference() {
+		return getPerimeter();
+	}
+
+	@Override
+	public double getPerimeter() {
+		return this.perimeter;
+	}
+
+	public double getDiameter() {
+		return this.diameter;
+	}
+
+	public double getRadius() {
+		return this.radius;
+	}
+
+	/////// SETTERS
+
+	@Override
+	public void setPerimeter(double newPerimeter) {
+
+	    double newRadius = newPerimeter / (2 * piValue);
+	    this.radius = newRadius;
+	    this.diameter = 2 * newRadius;
+	    this.area = piValue * (newRadius * newRadius);
+	    this.perimeter = newPerimeter;
+	}
+
+
+	@Override
+	public void setArea(double newArea) {
+
+	    double newRadius = Math.sqrt(newArea / piValue);
+	    this.radius = newRadius;
+	    this.diameter = 2 * newRadius;
+	    this.area = newArea;
+	  
+	    this.perimeter = 2 * (piValue * newRadius);
+	}
+
+
+}
